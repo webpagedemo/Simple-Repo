256ae495f7fe01f35c83500094d2ca8ea51eec56
Tomasz Pucek <tpucek@sum.edu.pl>
2019-09-16 15:26:16 +0200

    remove code

--- script.py
+++ script.py
@@ -1,9 +1,6 @@
 import sys
 import requests
 
-print(sys.version)
-print(sys.executable)
-
 
 def greet(who_to_greet):
     greeting = "Hello, {}".format(who_to_greet)
@@ -11,4 +8,4 @@ def greet(who_to_greet):
 
 
 r = requests.get("https://coreyms.com")
-print(r.status_code)
+# print(r.status_code)
